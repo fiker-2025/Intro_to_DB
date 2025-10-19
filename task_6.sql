bcacba2b8b82231059b3e072f36e8409cb985dd9
fiker-2025 <fikerteamenu9@gmail.com>
2025-10-20 00:11:52 +0300

    s

--- task_6.sql
+++ task_6.sql
@@ -2,7 +2,7 @@
 -- Insert multiple rows into the customer table
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES 
-(2, 'John Doe', 'johndoe@example.com', '456 Joy Street'),
-(3, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(4, 'Fikrte Amenu', 'fikerteamenu63@gmail.com', 'Addis Ababa');
+VALUES
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
